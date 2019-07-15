ee656ac7741a4e733cabf5756b724846ded75dad
00557012 <david02653+1@gmail.com>
2019-07-15 21:39:04 +0800

    upload new project structure

---
+++ src/padone/common/controller/ArticleListServlet.java
@@ -0,0 +1,72 @@
+package padone.common.controller;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import padone.common.model.ArticleServer;
+import com.google.gson.Gson;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.json.JSONArray;
+
+@WebServlet("/ArticleListServlet")
+public class ArticleListServlet extends HttpServlet {
+    public ArticleListServlet(){ super(); }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("application/json; charset=utf-8");
+        DataSource datasource = (DataSource)getServletContext().getAttribute("db");
+        Gson gson;
+        //super.doGet(req, resp);
+
+        // get all article from database
+        gson = new Gson();
+        response.getWriter().print(gson.toJson(ArticleServer.getAllArticle(datasource)));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        response.setContentType("application/json; charset=utf-8");
+        DataSource datasource = (DataSource)getServletContext().getAttribute("db");
+
+        String option = request.getParameter("option");
+        String target;
+        Gson gson;
+
+
+        if(option.equals("all")){
+            // get all article
+            gson = new Gson();
+            response.getWriter().print(gson.toJson(ArticleServer.getAllArticle(datasource)));
+        }else if(option.equals("author")){
+            gson = new Gson();
+            target = request.getParameter("name");
+            response.getWriter().print(gson.toJson(ArticleServer.getAuthorArticle(datasource, target)));
+            // get author article
+        }else if(option.equals("department")){
+            gson = new Gson();
+            target = request.getParameter("department");
+            response.getWriter().print(gson.toJson(ArticleServer.getCategoryArticle(datasource, target)));
+            // get department article
+        }else if(option.equals("articleID")){
+            gson = new Gson();
+            target = request.getParameter("id");
+            response.getWriter().print(gson.toJson(ArticleServer.getSpecificArticle(datasource, target)));
+            // get specific article
+        }else if(option.equals("singleTag")){
+            gson = new Gson();
+            target = request.getParameter("tag");
+            response.getWriter().print(gson.toJson(ArticleServer.getSingleTagArticle(datasource, target)));
+        }
+    }
+}
---
+++ src/padone/common/model/Article.java
@@ -0,0 +1,56 @@
+package padone.common.model;
+public class Article {
+	private String articleID;
+	private String title;
+	private String author;
+	private String department;
+	private String postTime;
+	private String lastUpdateTime;
+	private String description;
+	private String tag;
+	private String image;
+	public void setArticleID(String articleID) {
+		this.articleID=articleID;
+	}
+	public void setTitle(String title) {
+		this.title=title;
+	}
+	public void setAuthor(String author) {
+		this.author=author;
+	}
+	public void setImage(String image) {
+		this.image=image;
+	}
+	public void setPostTime(String postTime) {
+		this.postTime=postTime;
+	}
+	public void setLastUpdateTime(String lastUpdateTime) {
+		this.lastUpdateTime=lastUpdateTime;
+	}
+	public void setDescription(String descriptionD) {
+		this.description=descriptionD;
+	}
+	public void setTag(String tag) {
+		this. tag= tag;
+	}
+	public void setDepartment(String department) {
+		this.department=department;
+	}
+	public Article() {
+		
+	}
+	public Article(String articleID,String author,String department,String postTime,String lastUpdateTime,String description,String tag) {
+		setArticleID(articleID);
+		setAuthor(author);
+		setPostTime(postTime);
+		setLastUpdateTime(lastUpdateTime);
+		setDescription(description);
+		setTag(tag);
+		setDepartment(department);
+		
+	}
+	void tostring() {
+		System.out.println("articleID:"+articleID+",author:"+author+",department:"+department+",postTime:"+postTime+",lastUpdateTime:"+lastUpdateTime+",description:"+description+",tag:"+tag+",image:"+image+",title:"+title);
+	}
+	
+}
