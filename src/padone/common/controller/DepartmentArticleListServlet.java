a5230ccd08eac50cd8ab6ee4627bfdeb595d9e23
00557012 <david02653@gmail.com>
2019-07-20 14:26:05 +0800

    add authorArticle and departmentArticle servlet

---
+++ src/padone/common/controller/DepartmentArticleListServlet.java
@@ -0,0 +1,28 @@
+package padone.common.controller;
+
+import com.google.gson.Gson;
+import padone.common.model.ArticleServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+public class DepartmentArticleListServlet extends HttpServlet {
+    public DepartmentArticleListServlet(){ super(); }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        response.setContentType("application/json, charset=utf-8");
+        DataSource dataSource = (DataSource)getServletContext().getAttribute("db");
+        Gson gson;
+
+        String department = request.getParameter("department");
+
+        gson = new Gson();
+        response.getWriter().print(ArticleServer.getCategoryArticle(dataSource, department));
+    }
+}
