3b6568185ed55cb2ef54c4e257ae88d95136e1a4
00557012 <david02653@gmail.com>
2019-07-20 17:04:01 +0800

    add specific articleID servlet

--- src/padone/common/controller/SpecificArticleListServlet.java
+++ src/padone/common/controller/SpecificArticleListServlet.java
@@ -7,22 +7,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 
-public class DepartmentArticleListServlet extends HttpServlet {
-    public DepartmentArticleListServlet(){ super(); }
+import java.io.IOException;
+
+public class SpecificArticleListServlet extends HttpServlet {
+    public SpecificArticleListServlet(){ super(); }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
         response.setContentType("application/json; charset=utf-8");
         DataSource dataSource = (DataSource)getServletContext().getAttribute("db");
-        Gson gson;
+        Gson gson = new Gson();
+        String articleID = request.getParameter("articleID");
 
-        String department = request.getParameter("department");
-
-        gson = new Gson();
-        response.getWriter().print(gson.toJson(ArticleServer.getCategoryArticle(dataSource, department)));
+        response.getWriter().print(gson.toJson(ArticleServer.getSpecificArticle(dataSource, articleID)));
     }
 }
