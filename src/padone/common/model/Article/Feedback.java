2d7fb328181aec203f1c80f64f67fd50103d7b18
00557012 <david02653@gmail.com>
2019-09-11 17:20:48 +0800

    add author in to feedback JavaBean, add join to fix sql problem in feedback handler

--- src/padone/common/model/Article/Feedback.java
+++ src/padone/common/model/Article/Feedback.java
@@ -2,53 +2,47 @@ package padone.common.model.Article;
 
 public class Feedback {
 	private String articleID;
-	private String ID;     // feedback id
-	private String author; // author id
+	private String feedbackID;
+	private String author;
+	private String authorID;
 	private String message;
 	private String updateTime;
-	public void SetArticleID(String articleID) {
-		this.articleID=articleID;
+
+	public void setArticleID(String articleID){
+		this.articleID = articleID;
 	}
-	public void SetID(String id) {
-		ID=id;
-		
+
+	public void setFeedbackID(String feedbackID){
+		this.feedbackID = feedbackID;
 	}
-	public void SetAuthor(String author) {
-		this.author=author;
+
+	public void setAuthor(String author){
+		this.author = author;
 	}
-	public void SetMessage(String message) {
-		this.message=message;
+
+	public void setAuthorID(String authorID){
+		this.authorID = authorID;
 	}
-	public void SetUpdateTime(String updatetime) {
-		updateTime=updatetime;
+
+	public void setMessage(String message){
+		this.message = message;
 	}
-	public String GetArticleID() {
-		return articleID;
+
+	public void setUpdateTime(String updateTime){
+		this.updateTime = updateTime;
 	}
-	public String GetID() {
-		return ID;
-		
-	}
-	public String GetAuthor() {
-		return author;
-	}
-	public String GetMessage() {
-		return message;
-	}
-	public String GetUpdateTime() {
-		return updateTime;
-	}
-	public void feedback(String articleID,String id,String author,String message,String updatetime) {
-		SetArticleID(articleID);
-		SetID(id);
-		SetAuthor(author);
-		SetMessage(message);
-		SetUpdateTime(updatetime);
+
+	public void feedback(String articleID,String id, String author,String authorID,String message,String updatetime) {
+		setArticleID(articleID);
+		setFeedbackID(id);
+		setAuthor(author);
+		setAuthorID(authorID);
+		setMessage(message);
+		setUpdateTime(updatetime);
 	}
 	@Override
 	public String toString() {
-		return "articleID: "+articleID+"\n"+"ID: "+ID+"\n"+"author: "+author+"\n"+"message: "+message+"\n"+"update: "+updateTime+"\n";
-		
+		return "articleID: "+articleID+"\n"+"ID: "+feedbackID + "\n" + "author: " + author+"\n"+"authorID: "+authorID+"\n"+"message: "+message+"\n"+"update: "+updateTime+"\n";
 	}
 	
 
