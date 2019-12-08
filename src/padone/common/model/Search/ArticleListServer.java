dc63a9fa407b7f6ab2e21b4422a93dfeee6c5f62
00557012 <david02653@gmail.com>
2019-12-08 13:27:05 +0800

    fix sql problem in suggest status return

--- src/padone/common/model/Search/ArticleListServer.java
+++ src/padone/common/model/Search/ArticleListServer.java
@@ -252,7 +252,7 @@ public class ArticleListServer {
             gStmt.setString(2, userID);
             trs = gStmt.executeQuery();
 
-            gStmt = conn.prepareStatement("SELECT COUNT(DISTINCT articleID) FROM suggestArticle INNER JOIN (SELECT familyID FROM patientrelationship WHERE patientID = ?) as p ON userID = p.familyID and articleID = ?");
+            gStmt = conn.prepareStatement("SELECT COUNT(DISTINCT articleID) as num FROM suggestArticle INNER JOIN (SELECT familyID FROM patientrelationship WHERE patientID = ?) as p ON userID = p.familyID and articleID = ?");
             gStmt.setString(2, articleID);
             gStmt.setString(1, userID);
             frs = gStmt.executeQuery();
